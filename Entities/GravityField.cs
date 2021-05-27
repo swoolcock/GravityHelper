c6b659bd72785776922a5a154e5d55d1c13d3691
Shane Woolcock <samahnub@gmail.com>
2021-05-27 21:27:18 +0930

    Render arrows

--- Entities/GravityField.cs
+++ Entities/GravityField.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Celeste;
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Monocle;
 
 namespace GravityHelper.Entities
@@ -24,10 +25,10 @@ namespace GravityHelper.Entities
         public VisualType FieldType { get; }
         public bool AttachToSolids { get; }
 
-        private bool ShouldDrawArrows => !(ArrowType == VisualType.None || ArrowType == VisualType.Default && GravityType == GravityType.None); 
-        private bool ShouldDrawField => !(FieldType == VisualType.None || FieldType == VisualType.Default && GravityType == GravityType.None);
-        
-        public Color FieldColor => colorForGravityType(FieldType == VisualType.Default ? GravityType : (GravityType) FieldType);
+        public Color FieldColor => colorForGravityType(fieldGravityType);
+
+        private GravityType arrowGravityType => ArrowType == VisualType.Default ? GravityType : (GravityType) ArrowType;
+        private GravityType fieldGravityType => FieldType == VisualType.Default ? GravityType : (GravityType) FieldType;
 
         private static Color colorForGravityType(GravityType type) => type switch
         {
@@ -37,14 +38,20 @@ namespace GravityHelper.Entities
             _ => Color.White
         };
 
+        private readonly bool shouldDrawArrows; 
+        private readonly bool shouldDrawField;
+        private readonly MTexture arrowTexture;
+        private readonly MTexture arrowSmallTexture;
+        private readonly SpriteEffects arrowSpriteEffects;
+        private readonly Vector2 arrowOrigin;
+        private readonly Vector2 arrowSmallOrigin;
+        
         private readonly Hitbox normalHitbox;
         private readonly Hitbox staticMoverHitbox;
 
-        private List<Vector2> particles = new List<Vector2>();
-        private float[] speeds = { 12f, 20f, 40f };
+        private readonly List<Vector2> particles = new List<Vector2>();
+        private readonly float[] speeds = {12f, 20f, 40f};
         private GravityFieldGroup fieldGroup;
-
-        // public float Flashing => fieldGroup?.Flashing ?? 0f;
 
         public GravityField(EntityData data, Vector2 offset)
             : base(data, offset)
@@ -54,24 +61,56 @@ namespace GravityHelper.Entities
             ArrowType = (VisualType)data.Int("arrowType", (int) VisualType.Default);
             FieldType = (VisualType)data.Int("fieldType", (int) VisualType.Default);
 
-            Visible = ShouldDrawArrows || ShouldDrawField;
+            shouldDrawArrows = !(ArrowType == VisualType.None || ArrowType == VisualType.Default && GravityType == GravityType.None);
+            shouldDrawField = !(FieldType == VisualType.None || FieldType == VisualType.Default && GravityType == GravityType.None);
+
+            Visible = shouldDrawArrows || shouldDrawField;
             Collider = normalHitbox = new Hitbox(data.Width, data.Height);
+
+            if (shouldDrawArrows && !shouldDrawField)
+                Depth = Depths.FGDecals - 1;
+            else
+                Depth = Depths.Player + 1;
 
             staticMoverHitbox = new Hitbox(data.Width + 2, data.Height + 2, -1, -1);
 
             if (AttachToSolids)
+            {
                 Add(new StaticMover
                 {
-                    OnAttach = p => Depth = p.Depth + 1,
+                    OnAttach = p => Depth = p.Depth - 1,
                     SolidChecker = staticMoverCollideCheck
                 });
-
-            if (ShouldDrawArrows)
-            {
-
             }
 
-            if (ShouldDrawField)
+            if (shouldDrawArrows)
+            {
+                switch (arrowGravityType)
+                {
+                    case GravityType.Normal:
+                        arrowTexture = GFX.Game["objects/GravityHelper/gravityField/arrow"];
+                        arrowSmallTexture = GFX.Game["objects/GravityHelper/gravityField/arrowSmall"];
+                        arrowSpriteEffects = SpriteEffects.FlipVertically;
+                        break;
+                    
+                    case GravityType.Inverted:
+                        arrowTexture = GFX.Game["objects/GravityHelper/gravityField/arrow"];
+                        arrowSmallTexture = GFX.Game["objects/GravityHelper/gravityField/arrowSmall"];
+                        arrowSpriteEffects = SpriteEffects.None;
+                        break;
+                    
+                    case GravityType.Toggle:
+                        arrowTexture = GFX.Game["objects/GravityHelper/gravityField/doubleArrow"];
+                        arrowSmallTexture = GFX.Game["objects/GravityHelper/gravityField/doubleArrowSmall"];
+                        arrowSpriteEffects = SpriteEffects.None;
+                        break;
+                }
+                
+                arrowOrigin = new Vector2(arrowTexture.Width / 2f, arrowTexture.Height / 2f);
+                arrowSmallOrigin = new Vector2(arrowSmallTexture.Width / 2f, arrowSmallTexture.Height / 2f);
+            }
+
+            if (shouldDrawField)
             {
                 for (int index = 0; index < Width * (double) Height / 16.0; ++index)
                     particles.Add(new Vector2(Calc.Random.NextFloat(Width - 1f), Calc.Random.NextFloat(Height - 1f)));
@@ -122,7 +161,7 @@ namespace GravityHelper.Entities
         {
             base.Added(scene);
 
-            if (ShouldDrawField)
+            if (shouldDrawField)
             {
                 var renderer = getRenderer(scene, true);
                 renderer?.Track(this);
@@ -135,7 +174,7 @@ namespace GravityHelper.Entities
         {
             base.Removed(scene);
 
-            if (ShouldDrawField)
+            if (shouldDrawField)
             {
                 var renderer = getRenderer(scene);
                 renderer?.Untrack(this, true);
@@ -147,9 +186,7 @@ namespace GravityHelper.Entities
         public override void Awake(Scene scene)
         {
             base.Awake(scene);
-
-            if (ShouldDrawField)
-                buildFieldGroup();
+            buildFieldGroup();
         }
 
         public override void Update()
@@ -178,9 +215,41 @@ namespace GravityHelper.Entities
         {
             base.Render();
 
-            Color color = Color.White * 0.5f;
-            foreach (Vector2 particle in particles)
-                Draw.Pixel.Draw(Position + particle, Vector2.Zero, color);
+            if (shouldDrawField)
+            {
+                Color color = Color.White * 0.5f;
+                foreach (Vector2 particle in particles)
+                    Draw.Pixel.Draw(Position + particle, Vector2.Zero, color);
+            }
+
+            if (shouldDrawArrows)
+            {
+                int widthInTiles = (int) (Width / 8);
+                int heightInTiles = (int) (Height / 8);
+                
+                // one arrow every 2 tiles, rounded down, but at least one
+                int arrowsX = Math.Max(widthInTiles / 2, 1);
+                int arrowsY = Math.Max(heightInTiles / 2, 1);
+                
+                // if width or height is 1, scale down the arrows
+                var texture = widthInTiles == 1 || heightInTiles == 1 ? arrowSmallTexture : arrowTexture;
+                var origin = widthInTiles == 1 || heightInTiles == 1 ? arrowSmallOrigin : arrowOrigin;
+                var color = shouldDrawField ? Color.White * 0.5f : Color.White;
+                
+                // arrows should be centre aligned in each 2x2 box
+                // offset by half a tile if the width or height is odd
+                for (int y = 0; y < arrowsY; y++)
+                {
+                    int offsetY = y * 16 + 8 + heightInTiles % 2 * 4;
+                    if (heightInTiles == 1) offsetY = 4;
+                    for (int x = 0; x < arrowsX; x++)
+                    {
+                        int offsetX = x * 16 + 8 + widthInTiles % 2 * 4;
+                        if (widthInTiles == 1) offsetX = 4;
+                        texture.Draw(Position + new Vector2(offsetX, offsetY), origin, color, 1f, 0f, arrowSpriteEffects);
+                    }
+                }
+            }
         }
 
         private bool staticMoverCollideCheck(Solid solid)
