d557733aa9805237da56175af181a9562ab9c028
Shane Woolcock <samahnub@gmail.com>
2021-07-26 16:21:22 +0930

    Force hook exceptions to rethrow rather than silently failing

--- Hooks/HookUtils.cs
+++ Hooks/HookUtils.cs
@@ -11,6 +11,9 @@ namespace Celeste.Mod.GravityHelper.Hooks
         public static void LogCurrentMethod(string message, [CallerMemberName] string caller = null) =>
             Logger.Log(nameof(GravityHelperModule), $"{caller}: {message}");
 
+        public static void LogCurrentMethod(LogLevel logLevel, string message, [CallerMemberName] string caller = null) =>
+            Logger.Log(logLevel, nameof(GravityHelperModule), $"{caller}: {message}");
+
         public static void SafeHook(Action action, [CallerMemberName] string caller = null)
         {
             LogCurrentMethod("Hooking IL...", caller);
@@ -21,7 +24,8 @@ namespace Celeste.Mod.GravityHelper.Hooks
             }
             catch (HookException hookException)
             {
-                LogCurrentMethod(hookException.Message, caller);
+                LogCurrentMethod(LogLevel.Error, hookException.Message, caller);
+                throw;
             }
         }
     }
