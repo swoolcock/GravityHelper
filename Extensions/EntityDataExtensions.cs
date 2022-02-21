865173c9070f7368f25a5c7d60603a4d8e28a0d8
Shane Woolcock <samahnub@gmail.com>
2022-02-21 16:08:45 +1030

    Default to version 1.0

--- Extensions/EntityDataExtensions.cs
+++ Extensions/EntityDataExtensions.cs
@@ -7,14 +7,19 @@ namespace Celeste.Mod.GravityHelper.Extensions
 {
     public static class EntityDataExtensions
     {
-        public static Version ModVersion(this EntityData self) => self?.Version("modVersion");
-        public static Version PluginVersion(this EntityData self) => self?.Version("pluginVersion");
+        private const string mod_version_key = "modVersion";
+        private const string plugin_version_key = "pluginVersion";
 
-        public static Version Version(this EntityData self, string key) => self != null && self.TryVersion(key, out var value) ? value : null;
+        public static Version ModVersion(this EntityData self) => self.ModVersion(new Version(1, 0));
+        public static Version PluginVersion(this EntityData self) => self.PluginVersion(new Version(1, 0));
+        public static Version ModVersion(this EntityData self, Version defaultValue) => self.Version(mod_version_key, defaultValue);
+        public static Version PluginVersion(this EntityData self, Version defaultValue) => self.Version(plugin_version_key, defaultValue);
+
+        public static Version Version(this EntityData self, string key, Version defaultValue = default) => self.TryVersion(key, out var value) ? value : defaultValue;
 
         public static bool TryVersion(this EntityData self, string key, out Version version)
         {
-            var value = self?.Attr(key, string.Empty) ?? string.Empty;
+            var value = self.Attr(key, string.Empty) ?? string.Empty;
             return System.Version.TryParse(value, out version);
         }
     }
