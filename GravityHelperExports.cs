e0b169b25c62d5266fd6dc559d2979d628687df1
Shane Woolcock <samahnub@gmail.com>
2022-05-15 09:36:21 +0930

    Remove complicated exports

--- GravityHelperExports.cs
+++ GravityHelperExports.cs
@@ -2,9 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using Celeste.Mod.GravityHelper.Components;
-using Microsoft.Xna.Framework;
-using Monocle;
+using Celeste.Mod.GravityHelper.Extensions;
 using MonoMod.ModInterop;
 
 namespace Celeste.Mod.GravityHelper {
@@ -18,51 +16,16 @@ namespace Celeste.Mod.GravityHelper {
         public static int GetPlayerGravity() =>
             (int) (GravityHelperModule.PlayerComponent?.CurrentGravity ?? GravityType.Normal);
 
+        public static int GetActorGravity(Actor actor) => (int)(actor?.GetGravity() ?? GravityType.Normal);
+
         public static void SetPlayerGravity(int gravityType, float momentumMultiplier) =>
             GravityHelperModule.PlayerComponent?.SetGravity((GravityType) gravityType, momentumMultiplier);
 
+        public static void SetActorGravity(Actor actor, int gravityType, float momentumMultiplier) =>
+            actor?.SetGravity((GravityType)gravityType, momentumMultiplier);
+
         public static bool IsPlayerInverted() => GravityHelperModule.ShouldInvertPlayer;
 
-        public static Component CreateGravityComponent(
-            Action<int, bool, float> updatePosition,
-            Action<int, bool, float> updateColliders,
-            Action<int, bool, float> updateSpeed,
-            Action<int, bool, float> updateVisuals,
-            Func<Vector2> getSpeed,
-            Action<Vector2> setSpeed,
-            Func<bool> checkInvert)
-        {
-            static Action<GravityChangeArgs> makeClosure(Action<int, bool, float> action) =>
-                action == null ? null : args => action((int)args.NewValue, args.Changed, args.MomentumMultiplier);
-
-            return new GravityComponent
-            {
-                UpdatePosition = makeClosure(updatePosition),
-                UpdateColliders = makeClosure(updateColliders),
-                UpdateSpeed = makeClosure(updateSpeed),
-                UpdateVisuals = makeClosure(updateVisuals),
-                CheckInvert = checkInvert,
-                GetSpeed = getSpeed,
-                SetSpeed = setSpeed,
-            };
-        }
-
-        public static Component CreateEntityGravityListener(Entity entity, Action<Entity, int, bool, float> action) =>
-            new GravityListener(entity)
-            {
-                GravityChanged = (e, args) => action(e, (int)args.NewValue, args.Changed, args.MomentumMultiplier),
-            };
-
-        public static Component CreateTypeGravityListener(Type type, Action<Entity, int, bool, float> action) =>
-            new GravityListener(type)
-            {
-                GravityChanged = (e, args) => action(e, (int)args.NewValue, args.Changed, args.MomentumMultiplier),
-            };
-
-        public static Component CreatePlayerGravityListener(Action<Entity, int, bool, float> action) =>
-            new PlayerGravityListener
-            {
-                GravityChanged = (e, args) => action(e, (int)args.NewValue, args.Changed, args.MomentumMultiplier),
-            };
+        public static bool IsActorInverted(Actor actor) => actor?.ShouldInvert() ?? false;
     }
 }
