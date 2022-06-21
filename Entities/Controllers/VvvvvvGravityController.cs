c190cd1025e33d25cd22763b2142486260de68bd
Shane Woolcock <samahnub@gmail.com>
2022-06-21 21:47:19 +0930

    Add buffering to vvvvvv flips

--- Entities/Controllers/VvvvvvGravityController.cs
+++ Entities/Controllers/VvvvvvGravityController.cs
@@ -20,6 +20,10 @@ namespace Celeste.Mod.GravityHelper.Entities.Controllers
         public bool IsVvvvvv => Mode == VvvvvvMode.TriggerBased && GravityHelperModule.Session.VvvvvvTrigger || Mode == VvvvvvMode.On;
 
         private bool _dashDisabled;
+        private float _bufferTimeRemaining;
+        private bool _autoFlip;
+
+        private const float flip_buffer_seconds = 0.1f;
 
         public VvvvvvGravityController(EntityData data, Vector2 offset)
             : base(data, offset)
@@ -47,31 +51,64 @@ namespace Celeste.Mod.GravityHelper.Entities.Controllers
             var active = ActiveController;
             if (!active.IsVvvvvv) return;
 
-            var playerData = new DynData<Player>(player);
-            var onGround = player.OnGround() || playerData.Get<float>("jumpGraceTimer") > 0;
-            var dreamJump = playerData.Get<bool>("dreamJump");
+            // greedily consume all jumps
             var jumpPressed = Input.Jump.Pressed;
             Input.Jump.ConsumePress();
 
+            // do nothing if we don't press jump
+            if (!jumpPressed) return;
+
+            if (canFlip(player, out var jumpBuffer))
+            {
+                // consume an Extended Variant Jump(TM)
+                if (jumpBuffer > 0)
+                    ReflectionCache.ExtendedVariantsJumpCountSetJumpCountMethodInfo?.Invoke(null, new object[] { jumpBuffer - 1, false });
+                // do the flip right now
+                doFlip(player);
+            }
+            else
+            {
+                // delay the flip until we're able to
+                _autoFlip = true;
+                _bufferTimeRemaining = flip_buffer_seconds;
+            }
+        }
+
+        private bool canFlip(Player player, out int jumpBuffer)
+        {
+            // assume no extended variants jumps
+            jumpBuffer = 0;
+
             // for now we'll only allow normal state
-            if (player.StateMachine != Player.StNormal) return;
+            if (player.StateMachine != Player.StNormal) return false;
+
+            var playerData = new DynData<Player>(player);
+
+            // never allow from dream jumps
+            if (playerData.Get<bool>("dreamJump")) return false;
+
+            // on ground or within coyote frames
+            var onGround = player.OnGround() || playerData.Get<float>("jumpGraceTimer") > 0;
 
             // see if an Extended Variant Jump(TM) is available
-            int jumpBuffer = 0;
             if (ReflectionCache.ExtendedVariantsJumpCountGetJumpBufferMethodInfo != null)
                 jumpBuffer = (int)ReflectionCache.ExtendedVariantsJumpCountGetJumpBufferMethodInfo.Invoke(null, new object[0]);
 
-            if (jumpPressed && (onGround || jumpBuffer > 0) && !dreamJump)
-            {
-                // consume an Extended Variant Jump(TM)
-                if (!onGround && jumpBuffer > 0)
-                    ReflectionCache.ExtendedVariantsJumpCountSetJumpCountMethodInfo?.Invoke(null, new object[] { jumpBuffer - 1, false });
+            // we can flip if we're on the ground or have an extended variant jump
+            return onGround || jumpBuffer > 0;
+        }
 
-                GravityHelperModule.PlayerComponent?.SetGravity(GravityType.Toggle);
-                player.Speed.Y = 160f * (player.SceneAs<Level>().InSpace ? 0.6f : 1f);
-                if (!string.IsNullOrEmpty(active.FlipSound))
-                    Audio.Play(active.FlipSound);
-            }
+        private void doFlip(Player player)
+        {
+            _autoFlip = false;
+            _bufferTimeRemaining = 0f;
+
+            var active = ActiveController;
+
+            GravityHelperModule.PlayerComponent?.SetGravity(GravityType.Toggle);
+            player.Speed.Y = 160f * (player.SceneAs<Level>().InSpace ? 0.6f : 1f);
+            if (!string.IsNullOrEmpty(active.FlipSound))
+                Audio.Play(active.FlipSound);
         }
 
         public override void Update()
@@ -85,6 +122,13 @@ namespace Celeste.Mod.GravityHelper.Entities.Controllers
             {
                 _dashDisabled = active.IsVvvvvv && active.DisableDash;
                 updateInventory();
+            }
+
+            if (_bufferTimeRemaining > 0)
+            {
+                _bufferTimeRemaining -= Engine.DeltaTime;
+                if (_autoFlip && Scene.Tracker.GetEntity<Player>() is { } player && canFlip(player, out _))
+                    doFlip(player);
             }
         }
 
