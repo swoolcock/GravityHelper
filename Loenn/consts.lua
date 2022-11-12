e1898446d5ce45de4f09f4c0d9301b1a67eb57f1
Shane Woolcock <samahnub@gmail.com>
2022-11-12 15:42:25 +1030

    Bump version

--- Loenn/consts.lua
+++ Loenn/consts.lua
@@ -16,7 +16,7 @@ local function makeOptions(options, defaults, ...)
 end
 
 local consts = {
-    modVersion = "1.1.25",
+    modVersion = "1.1.26",
     ignoredFields = {
         "modVersion",
         "pluginVersion",
