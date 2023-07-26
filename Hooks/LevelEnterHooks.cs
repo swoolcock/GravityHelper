401268e75af58699ef0e186d2194bdc6e68ea4e4
Shane Woolcock <samahnub@gmail.com>
2023-07-26 16:02:15 +0930

    Add UDJT corner correction warning postcard

--- Hooks/LevelEnterHooks.cs
+++ Hooks/LevelEnterHooks.cs
@@ -2,13 +2,18 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections;
+using System.Linq;
 
 namespace Celeste.Mod.GravityHelper.Hooks
 {
     public static class LevelEnterHooks
     {
+        private static bool _hasGravityHelper;
         private static bool _hasVvvvvv;
+        private static bool _hasMHHUDJT;
+
         private static Postcard _vvvvvvPostcard;
+        private static Postcard _udjtCorrectionPostcard;
 
         public static void Load()
         {
@@ -30,6 +35,7 @@ namespace Celeste.Mod.GravityHelper.Hooks
         {
             orig(self);
             _vvvvvvPostcard?.BeforeRender();
+            _udjtCorrectionPostcard?.BeforeRender();
         }
 
         private static IEnumerator LevelEnter_Routine(On.Celeste.LevelEnter.orig_Routine orig, LevelEnter self)
@@ -50,23 +56,57 @@ namespace Celeste.Mod.GravityHelper.Hooks
                 _vvvvvvPostcard = null;
             }
 
+            if (!_hasGravityHelper && _hasMHHUDJT && GravityHelperModule.Settings.AllowInAllMaps)
+            {
+                self.Add(_udjtCorrectionPostcard = new Postcard(Dialog.Clean("GRAVITYHELPER_POSTCARD_UDJT_CORRECTION"),
+                    "event:/ui/main/postcard_csides_in", "event:/ui/main/postcard_csides_out"));
+                yield return _udjtCorrectionPostcard.DisplayRoutine();
+                _udjtCorrectionPostcard = null;
+            }
+
             yield return new SwapImmediately(orig(self));
         }
 
         private static void LevelEnter_Go(On.Celeste.LevelEnter.orig_Go orig, Session session, bool fromsavedata)
         {
-            checkForVvvvvv(session);
+            var found = checkForEntities(session, "GravityHelper/", "GravityHelper/VvvvvvGravityController", "MaxHelpingHand/UpsideDownJumpThru");
+            _hasGravityHelper = found[0];
+            _hasVvvvvv = found[1];
+            _hasMHHUDJT = found[2];
+
             orig(session, fromsavedata);
         }
 
-        private static void checkForVvvvvv(Session session)
+        private static bool[] checkForEntities(Session session, params string[] entityNames)
         {
+            var rv = new bool[entityNames.Length];
             if (AreaData.Areas.Count <= session.Area.ID ||
                 AreaData.Areas[session.Area.ID].Mode.Length <= (int)session.Area.Mode ||
                 AreaData.Areas[session.Area.ID].Mode[(int)session.Area.Mode] == null)
-                return;
+                return rv;
 
-            _hasVvvvvv = session.MapData.Levels.Exists(levelData => levelData.Entities.Exists(e => e.Name == "GravityHelper/VvvvvvGravityController"));
+            // check whether the requested entity names are just mod names
+            var wildcards = entityNames.Select(n => n.EndsWith("/")).ToArray();
+
+            // loop through (potentially) every entity in the map
+            int found = 0;
+            foreach (var e in session.MapData.Levels.SelectMany(levelData => levelData.Entities))
+            {
+                // check the entity names or mods we're looking for
+                for (int i = 0; i < entityNames.Length; i++)
+                {
+                    if (!rv[i] && (wildcards[i] ? e.Name.StartsWith(entityNames[i]) : e.Name == entityNames[i]))
+                    {
+                        rv[i] = true;
+                        found++;
+                    }
+                }
+                // if we've found all of them at least once, we don't need to check any more
+                if (found >= entityNames.Length)
+                    break;
+            }
+
+            return rv;
         }
     }
 }
