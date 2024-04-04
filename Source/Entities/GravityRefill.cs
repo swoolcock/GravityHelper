3d6beaa4bb4d90637baa1d7858573e4f10c211eb
Shane Woolcock <samahnub@gmail.com>
2024-04-04 22:13:49 +1030

    Add gravity refill walls

--- Source/Entities/GravityRefill.cs
+++ Source/Entities/GravityRefill.cs
@@ -5,12 +5,14 @@ using System;
 using System.Collections;
 using Celeste.Mod.Entities;
 using Celeste.Mod.GravityHelper.Extensions;
+using JetBrains.Annotations;
 using Microsoft.Xna.Framework;
 using Monocle;
 
 namespace Celeste.Mod.GravityHelper.Entities;
 
-[CustomEntity("GravityHelper/GravityRefill")]
+[CustomEntity("GravityHelper/GravityRefill = CreateRefill",
+    "GravityHelper/GravityRefillWall = CreateWall")]
 public class GravityRefill : Entity
 {
     // properties
@@ -65,8 +67,12 @@ public class GravityRefill : Entity
     private float _arrowIntervalOffset;
 
     private bool _emitNormal;
+    private bool _isWall;
+    private float _wallAlpha = 0.8f;
+    private WallRenderMode _wallRenderMode;
 
     private const string gravity_toggle_charges = "GravityHelper_toggle_charges";
+
     public static int NumberOfCharges
     {
         get => (Engine.Scene as Level)?.Session.GetCounter(gravity_toggle_charges) ?? 0;
@@ -85,11 +91,21 @@ public class GravityRefill : Entity
         init((int)(position.X + position.Y));
     }
 
-    public GravityRefill(EntityData data, Vector2 offset)
+    [UsedImplicitly]
+    public static Entity CreateRefill(Level level, LevelData levelData, Vector2 offset, EntityData entityData) =>
+        new GravityRefill(entityData, offset, false);
+
+    [UsedImplicitly]
+    public static Entity CreateWall(Level level, LevelData levelData, Vector2 offset, EntityData entityData) =>
+        new GravityRefill(entityData, offset, true);
+
+    public GravityRefill(EntityData data, Vector2 offset, bool wall)
         : base(data.Position + offset)
     {
         _modVersion = data.ModVersion();
         _pluginVersion = data.PluginVersion();
+
+        _isWall = wall;
 
         Charges = data.Int("charges", 1);
         Dashes = data.Int("dashes", -1);
@@ -97,14 +113,15 @@ public class GravityRefill : Entity
         RefillsDash = data.Bool("refillsDash", true);
         RefillsStamina = data.Bool("refillsStamina", true);
         RespawnTime = data.Float("respawnTime", 2.5f);
+        _wallAlpha = data.Float("wallAlpha", 0.8f);
 
-        init(data.ID);
+        init(data.ID, data.Width, data.Height);
     }
 
-    private void init(int randomSeed)
+    private void init(int randomSeed, int width = 0, int height = 0)
     {
-        Collider = new Hitbox(16f, 16f, -8f, -8f);
-        Depth = Depths.Pickups;
+        Collider = _isWall ? new Hitbox(width, height) : new Hitbox(16f, 16f, -8f, -8f);
+        Depth = _isWall ? Depths.TheoCrystal : Depths.Pickups;
 
         var path = "objects/GravityHelper/gravityRefill";
         var outlineName = Dashes == 2 ? "outline_two_dash" : "outline";
@@ -112,26 +129,30 @@ public class GravityRefill : Entity
 
         // add components
         Add(new PlayerCollider(OnPlayer),
-            _outline = new Image(GFX.Game[$"{path}/{outlineName}"]) {Visible = false},
             _sprite = GFX.SpriteBank.Create("gravityRefill"),
             _arrows = GFX.SpriteBank.Create("gravityRefillArrows"),
-            _wiggler = Wiggler.Create(1f, 4f, v => _sprite.Scale = Vector2.One * (float) (1.0 + (double) v * 0.2)),
-            new MirrorReflection(),
-            _bloom = new BloomPoint(0.8f, 16f),
-            _light = new VertexLight(Color.White, 1f, 16, 48),
-            _sine = new SineWave(0.6f, 0.0f));
+            _wiggler = Wiggler.Create(1f, 4f, v => _sprite.Scale = Vector2.One * (float)(1.0 + v * 0.2)),
+            new MirrorReflection());
 
-        _outline.CenterOrigin();
+        if (!_isWall)
+        {
+            Add(_bloom = new BloomPoint(0.8f, 16f),
+                _light = new VertexLight(Color.White, 1f, 16, 48),
+                _sine = new SineWave(0.6f, 0.0f),
+                _outline = new Image(GFX.Game[$"{path}/{outlineName}"]) { Visible = false });
+
+            _outline.CenterOrigin();
+        }
 
         // this uses a random sample but so as to not break existing maps i'm leaving it above the PushRandomDisposable
         _sprite.Play(animationName, true, true);
 
         using var _ = new PushRandomDisposable(randomSeed);
-        _sine.Randomize();
+        _sine?.Randomize();
         _arrows.OnFinish = _ => _arrows.Visible = false;
         _arrowIntervalOffset = Calc.Random.NextFloat(2f);
 
-        updateY();
+        updateSpritePos();
     }
 
     public override void Added(Scene scene)
@@ -150,7 +171,7 @@ public class GravityRefill : Entity
             if (_respawnTimeRemaining <= 0.0)
                 respawn();
         }
-        else if (Scene.OnInterval(0.1f))
+        else if (Scene.OnInterval(0.1f) && !_isWall)
         {
             var offset = Vector2.UnitY * (_emitNormal ? 5f : -5f);
             var range = Vector2.One * 4f;
@@ -160,16 +181,20 @@ public class GravityRefill : Entity
             _emitNormal = !_emitNormal;
         }
 
-        updateY();
+        updateSpritePos();
 
-        _light.Alpha = Calc.Approach(_light.Alpha, _sprite.Visible ? 1f : 0.0f, 4f * Engine.DeltaTime);
-        _bloom.Alpha = _light.Alpha * 0.8f;
+        if (_light != null && _bloom != null)
+        {
+            _light.Alpha = Calc.Approach(_light.Alpha, _sprite.Visible ? 1f : 0.0f, 4f * Engine.DeltaTime);
+            _bloom.Alpha = _light.Alpha * 0.8f;
+        }
 
-        if (!Scene.OnInterval(2f, _arrowIntervalOffset) || !_sprite.Visible) return;
-
-        var arrowName = Dashes == 2 ? "arrows_two_dash" : "arrows";
-        _arrows.Play(arrowName, true);
-        _arrows.Visible = true;
+        if (Scene.OnInterval(2f, _arrowIntervalOffset) && _sprite.Visible && (!_isWall || _wallRenderMode == WallRenderMode.Filled))
+        {
+            var arrowName = Dashes == 2 ? "arrows_two_dash" : "arrows";
+            _arrows.Play(arrowName, true);
+            _arrows.Visible = true;
+        }
     }
 
     private void respawn()
@@ -177,21 +202,107 @@ public class GravityRefill : Entity
         if (Collidable) return;
         Collidable = true;
 
+        _wallRenderMode = WallRenderMode.Filled;
+
         _sprite.Visible = true;
-        _outline.Visible = false;
         _arrows.Stop();
-        Depth = Depths.Pickups;
-        _wiggler.Start();
-        Audio.Play("event:/game/general/diamond_return", Position);
-        _level.ParticlesFG.Emit(P_Regen, 16, Position, Vector2.One * 2f);
+
+        if (_outline != null)
+            _outline.Visible = false;
+
+        Depth = _isWall ? Depths.TheoCrystal : Depths.Pickups;
+
+        _wiggler?.Start();
+        Audio.Play("event:/game/general/diamond_return", Center);
+
+        _level.ParticlesFG.Emit(P_Regen, 16, Center, Vector2.One * 2f);
     }
 
-    private void updateY() => _arrows.Y = _sprite.Y = _bloom.Y = _sine.Value * 2f;
+    private void updateSpritePos()
+    {
+        if (_isWall)
+            _arrows.Position = _sprite.Position = Collider.Center;
+        else
+            _arrows.Y = _sprite.Y = _bloom.Y = _sine.Value * 2f;
+    }
 
     public override void Render()
     {
-        if (_sprite.Visible)
-            _sprite.DrawOutline();
+        if (!_isWall)
+        {
+            if (_sprite.Visible)
+                _sprite.DrawOutline();
+            base.Render();
+            return;
+        }
+
+        Camera camera = SceneAs<Level>().Camera;
+        if (Right < (double)camera.Left || Left > (double)camera.Right || Bottom < (double)camera.Top ||
+            Top > (double)camera.Bottom)
+            return;
+
+        getColors(out var fillColor, out var borderColor);
+
+        // make sure the sprites are opaque
+        _arrows.Color = _sprite.Color = Color.White;
+
+        switch (_wallRenderMode)
+        {
+            case WallRenderMode.Flash when !OneUse:
+            case WallRenderMode.Filled when !OneUse:
+                // draw outline 1 pixel out
+                Draw.HollowRect(X - 1f, Y - 1f, Width + 2f, Height + 2f, borderColor);
+
+                // draw filled rectangle 1 pixel in
+                Draw.Rect(X + 1f, Y + 1f, Width - 2f, Height - 2f, fillColor);
+
+                break;
+
+            case WallRenderMode.Flash when OneUse:
+            case WallRenderMode.Filled when OneUse:
+                // draw a translucent outline 1 pixel out
+                Draw.HollowRect(X - 1f, Y - 1f, Width + 2f, Height + 2f, borderColor * 0.6f);
+
+                // draw horizontal segments 1 pixel out
+                for (int index = 0; index < (double)Width; index += 8)
+                {
+                    Draw.Line(TopLeft - Vector2.UnitY + Vector2.UnitX * (index + 2), TopLeft - Vector2.UnitY + Vector2.UnitX * (index + 6), borderColor);
+                    Draw.Line(BottomLeft + Vector2.UnitX * (index + 2), BottomLeft + Vector2.UnitX * (index + 6), borderColor);
+                }
+
+                // draw vertical segments 1 pixel out
+                for (int index = 0; index < (double)Height; index += 8)
+                {
+                    Draw.Line(TopLeft + Vector2.UnitY * (index + 2), TopLeft + Vector2.UnitY * (index + 6), borderColor);
+                    Draw.Line(TopRight + Vector2.UnitX + Vector2.UnitY * (index + 2), TopRight + Vector2.UnitX + Vector2.UnitY * (index + 6), borderColor);
+                }
+
+                // draw filled rectangle 1 pixel in
+                Draw.Rect(X + 1f, Y + 1f, Width - 2f, Height - 2f, fillColor);
+
+                break;
+
+            case WallRenderMode.Returning:
+                // draw horizontal segments
+                for (int index = 0; index < (double)Width; index += 8)
+                {
+                    Draw.Line(TopLeft + Vector2.UnitX * (index + 2), TopLeft + Vector2.UnitX * (index + 6), borderColor);
+                    Draw.Line(BottomLeft - Vector2.UnitY + Vector2.UnitX * (index + 2), BottomLeft - Vector2.UnitY + Vector2.UnitX * (index + 6), borderColor);
+                }
+
+                // draw vertical segments
+                for (int index = 0; index < (double)Height; index += 8)
+                {
+                    Draw.Line(TopLeft + Vector2.UnitX + Vector2.UnitY * (index + 2), TopLeft + Vector2.UnitX + Vector2.UnitY * (index + 6), borderColor);
+                    Draw.Line(TopRight + Vector2.UnitY * (index + 2), TopRight + Vector2.UnitY * (index + 6), borderColor);
+                }
+
+                // set alpha on the sprites
+                _arrows.Color = _sprite.Color = Color.White * 0.25f;
+
+                break;
+        }
+
         base.Render();
     }
 
@@ -199,8 +310,8 @@ public class GravityRefill : Entity
     {
         var dashes = Dashes < 0 ? player.MaxDashes : Dashes;
         bool canUse = RefillsDash && player.Dashes < dashes ||
-            RefillsStamina && player.Stamina < 20 ||
-            NumberOfCharges < Charges;
+                      RefillsStamina && player.Stamina < 20 ||
+                      NumberOfCharges < Charges;
 
         if (!canUse) return;
 
@@ -217,6 +328,7 @@ public class GravityRefill : Entity
         Collidable = false;
         Add(new Coroutine(refillRoutine(player)));
         _respawnTimeRemaining = RespawnTime;
+        _wallRenderMode = WallRenderMode.Flash;
     }
 
     private IEnumerator refillRoutine(Player player)
@@ -225,19 +337,68 @@ public class GravityRefill : Entity
         Celeste.Freeze(0.05f);
         yield return null;
 
+        _wallRenderMode = WallRenderMode.Returning;
         refill._level.Shake();
-        refill._sprite.Visible = refill._arrows.Visible = false;
-        if (!refill.OneUse)
+        refill._arrows.Visible = false;
+        refill._sprite.Visible = _isWall;
+        if (!refill.OneUse && refill._outline != null)
             refill._outline.Visible = true;
         refill.Depth = Depths.BGDecals - 1;
         yield return 0.05f;
 
-        float direction = player.Speed.Angle();
-        refill._level.ParticlesFG.Emit(P_Shatter, 5, refill.Position, Vector2.One * 4f, direction - (float)Math.PI / 2f);
-        refill._level.ParticlesFG.Emit(P_Shatter, 5, refill.Position, Vector2.One * 4f, direction + (float)Math.PI / 2f);
-        SlashFx.Burst(refill.Position, direction);
+        if (!_isWall)
+        {
+            float direction = player.Speed.Angle();
+            refill._level.ParticlesFG.Emit(P_Shatter, 5, refill.Position, Vector2.One * 4f, direction - (float)Math.PI / 2f);
+            refill._level.ParticlesFG.Emit(P_Shatter, 5, refill.Position, Vector2.One * 4f, direction + (float)Math.PI / 2f);
+            SlashFx.Burst(refill.Position, direction);
+        }
 
         if (refill.OneUse)
             refill.RemoveSelf();
+    }
+
+    private void getColors(out Color fillColor, out Color borderColor)
+    {
+        switch (_wallRenderMode)
+        {
+            case WallRenderMode.Filled:
+                if (Dashes == 2)
+                {
+                    borderColor = Color.Orchid * _wallAlpha;
+                    fillColor = Color.DarkOrchid * _wallAlpha;
+                }
+                else if (!RefillsDash)
+                {
+                    borderColor = Color.LightSkyBlue * _wallAlpha;
+                    fillColor = Color.CornflowerBlue * _wallAlpha;
+                }
+                else
+                {
+                    borderColor = Color.BlueViolet * _wallAlpha;
+                    fillColor = Color.Indigo * _wallAlpha;
+                }
+
+                break;
+
+            case WallRenderMode.Flash:
+                fillColor = borderColor = Color.White * _wallAlpha;
+                break;
+
+            case WallRenderMode.Returning:
+                borderColor = Color.Gray * (_wallAlpha * 0.25f);
+                fillColor = Color.Transparent;
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private enum WallRenderMode
+    {
+        Filled,
+        Flash,
+        Returning,
     }
 }
